08dc4e7e3d886b1492e975aedf7a79bcbaad4a79
Philip <phfi@itu.dk>
2022-02-22 01:20:03 +0100

    api change

--- pages/api/resorts.js
+++ pages/api/resorts.js
@@ -1,9 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import fs from 'fs'
+import path from 'path'
+
 
 export default function handler(req, res) {
 
-  const resorts = JSON.parse(fs.readFileSync('./data/resorts.json', 'utf8'))
+  const dataDirectory = path.join(process.cwd(), 'data/resorts.json')
+
+  const resorts = JSON.parse(fs.readFileSync(dataDirectory, 'utf8'))
 
   res.status(200).json(resorts)
 }
